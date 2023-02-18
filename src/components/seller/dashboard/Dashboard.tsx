bfaa6ab086aa9ced0dafc1a8d83ef1f231e70e9e
Partha Priyadarshi Das <partha.das36@gmail.com>
2023-02-19 01:19:26 +0530

    fix seller dashboard

--- src/components/seller/dashboard/Dashboard.tsx
+++ src/components/seller/dashboard/Dashboard.tsx
@@ -92,16 +92,16 @@ const Dashboard = ({ onClick }: DashboardProps) => {
           </div>
         </main>
       </div>
-      <div className="flex w-full items-center justify-center px-2 py-16 sm:px-0">
+      <div className="flex w-full items-center justify-center px-2 sm:px-0">
         <div className="w-full">
           <Tab.Group>
-            <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+            <Tab.List className="flex space-x-1 bg-blue-900/20 p-1">
               {Object.keys(SellerDashboardCategory).map((category) => (
                 <Tab
                   key={category}
                   className={({ selected }) =>
                     classNames(
-                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                      "w-full py-2.5 text-sm font-medium leading-5 text-blue-700",
                       "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
                       selected
                         ? "bg-white shadow"
@@ -118,7 +118,7 @@ const Dashboard = ({ onClick }: DashboardProps) => {
                 <Tab.Panel
                   key={idx}
                   className={classNames(
-                    "rounded-xl bg-white p-3",
+                    "bg-white p-3",
                     "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
                   )}
                 >
