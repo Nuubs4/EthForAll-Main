3e08a30102e07f739ca71410a100015c9186a61b
Partha Priyadarshi Das <partha.das36@gmail.com>
2023-02-18 21:14:56 +0530

    update categories page

--- src/components/client/Categories.tsx
+++ src/components/client/Categories.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Fragment, useState } from "react"
+import { useRouter } from "next/router"
 
 import { Dialog, Disclosure, Transition } from "@headlessui/react"
 import { ChevronDownIcon, PlusIcon } from "@heroicons/react/20/solid"
@@ -7,105 +8,59 @@ import { XMarkIcon } from "@heroicons/react/24/outline"
 
 const filters = [
   {
-    id: "color",
-    name: "Color",
+    id: "by price",
+    name: "By Price",
     options: [
-      { value: "white", label: "White" },
-      { value: "beige", label: "Beige" },
-      { value: "blue", label: "Blue" },
-      { value: "brown", label: "Brown" },
-      { value: "green", label: "Green" },
-      { value: "purple", label: "Purple" },
+      { value: "how to High", label: "Low to High" },
+      { value: "high to Low", label: "High to Low" },
     ],
   },
   {
-    id: "category",
-    name: "Category",
+    id: "by category",
+    name: "By Category",
     options: [
-      { value: "new-arrivals", label: "All New Arrivals" },
-      { value: "tees", label: "Tees" },
-      { value: "crewnecks", label: "Crewnecks" },
-      { value: "sweatshirts", label: "Sweatshirts" },
-      { value: "pants-shorts", label: "Pants & Shorts" },
-    ],
-  },
-  {
-    id: "sizes",
-    name: "Sizes",
-    options: [
-      { value: "xs", label: "XS" },
-      { value: "s", label: "S" },
-      { value: "m", label: "M" },
-      { value: "l", label: "L" },
-      { value: "xl", label: "XL" },
-      { value: "2xl", label: "2XL" },
+      { value: "Top sellers", label: "Top sellers" },
+      { value: "Verified Sellers", label: "Verified Sellers" },
+      { value: "Seller Level 1", label: "Seller Level 1" },
+      { value: "Seller Level 2", label: "Seller Level 2" },
+      { value: "Seller Level 3", label: "Seller Level 3" },
     ],
   },
 ]
 const products = [
   {
     id: 1,
-    name: "Basic Tee 8-Pack",
+    name: "Accountability",
+    description: "Forward Branding Assistant",
     href: "#",
-    price: "$256",
-    description:
-      "Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.",
-    options: "8 colors",
-    imageSrc: "https://api.lorem.space/image/book?w=277&h=555&hash=gmu7h3nw",
-    imageAlt:
-      "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+    rating: "⭐⭐⭐⭐",
+    buyPrice: "256 BTC",
+    hirePrice: "360 BTC",
+    imageSrc: "https://api.lorem.space/image/fashion?w=277&h=555&hash=ipy4sigo",
+    imageAlt: "International Marketing Associate",
   },
   {
     id: 2,
-    name: "Basic Tee",
+    name: "Metrics",
+    description: "Chief Tactics Analyst",
     href: "#",
-    price: "$32",
-    description: "Look like a visionary CEO and wear the same black t-shirt every day.",
-    options: "Black",
-    imageSrc: "https://api.lorem.space/image/book?w=277&h=555&hash=gzdqknd9",
-    imageAlt: "Front of plain black t-shirt.",
+    rating: "⭐⭐⭐",
+    buyPrice: "256 BTC",
+    hirePrice: "360 BTC",
+    imageSrc: "https://api.lorem.space/image/fashion?w=277&h=555&hash=ifrggs7j",
+    imageAlt: "Global Security Architect",
   },
   {
-    id: 2,
-    name: "Basic Tee",
+    id: 3,
+    name: "Identity",
+    description: "Customer Operations Technician",
     href: "#",
-    price: "$32",
-    description: "Look like a visionary CEO and wear the same black t-shirt every day.",
-    options: "Black",
-    imageSrc: "https://api.lorem.space/image/book?w=277&h=555&hash=1oqtsp8t",
-    imageAlt: "Front of plain black t-shirt.",
+    rating: "⭐⭐⭐⭐⭐",
+    buyPrice: "256 BTC",
+    hirePrice: "360 BTC",
+    imageSrc: "https://api.lorem.space/image/fashion?w=277&h=555&hash=44acrzws",
+    imageAlt: "Customer Accounts Planner",
   },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    price: "$32",
-    description: "Look like a visionary CEO and wear the same black t-shirt every day.",
-    options: "Black",
-    imageSrc: "https://api.lorem.space/image/book?w=277&h=555&hash=lvcu1q15",
-    imageAlt: "Front of plain black t-shirt.",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    price: "$32",
-    description: "Look like a visionary CEO and wear the same black t-shirt every day.",
-    options: "Black",
-    imageSrc: "https://api.lorem.space/image/book?w=277&h=555&hash=dms4h9cm",
-    imageAlt: "Front of plain black t-shirt.",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    price: "$32",
-    description: "Look like a visionary CEO and wear the same black t-shirt every day.",
-    options: "Black",
-    imageSrc: "https://api.lorem.space/image/book?w=277&h=555&hash=2zxr5tcx",
-    imageAlt: "Front of plain black t-shirt.",
-  },
-  // More products...
 ]
 
 function classNames(...classes: any[]) {
@@ -114,6 +69,7 @@ function classNames(...classes: any[]) {
 
 const Categories = () => {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+  const router = useRouter()
 
   return (
     <div className="bg-white">
@@ -217,13 +173,12 @@ const Categories = () => {
         <main className="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8">
           <div className="border-b border-gray-200 pt-8 pb-10">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">Best seller for SEO</h1>
-            <p className="mt-4 text-base text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, natus assumenda!
-              Dolorum dicta repellendus praesentium atque iste nobis?
+            <p className="mt-4 text-2xl text-gray-500">
+              Services that people loved for superb work and delivery
             </p>
           </div>
 
-          <div className="pt-12 pb-24 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
+          <div className="pt-12 pb-24 lg:grid lg:grid-cols-3 xl:grid-cols-4">
             <aside>
               <h2 className="sr-only">Filters</h2>
 
@@ -278,33 +233,50 @@ const Categories = () => {
                 Products
               </h2>
 
-              <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:gap-x-8 xl:grid-cols-3">
+              <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-2 sm:gap-y-10  xl:grid-cols-3">
                 {products.map((product) => (
-                  <div
+                  <article
+                    onClick={() => {
+                      router.push("categories/overview")
+                    }}
                     key={product.id}
-                    className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+                    className="mx-auto w-full transform cursor-pointer py-6 px-3 duration-500 hover:-translate-y-1"
                   >
-                    <div className="aspect-w-3 aspect-h-4 sm:aspect-none bg-gray-200 group-hover:opacity-75 sm:h-96">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="h-full w-full object-cover object-center sm:h-full sm:w-full"
-                      />
-                    </div>
-                    <div className="flex flex-1 flex-col space-y-2 p-4">
-                      <h3 className="text-sm font-medium text-gray-900">
-                        <a href={product.href}>
-                          <span aria-hidden="true" className="absolute inset-0" />
+                    <div className="overflow-hidden rounded-lg border-4 bg-white shadow-xl">
+                      <div
+                        className="h-60 bg-cover bg-center object-scale-down object-top p-4"
+                        style={{
+                          backgroundImage: `url(${product.imageSrc})`,
+                        }}
+                      ></div>
+                      <div className="p-4 py-5">
+                        {/* job name */}
+                        <p className="text-2xl font-bold uppercase tracking-wide text-gray-700">
                           {product.name}
-                        </a>
-                      </h3>
-                      <p className="text-sm text-gray-500">{product.description}</p>
-                      <div className="flex flex-1 flex-col justify-end">
-                        <p className="text-sm italic text-gray-500">{product.options}</p>
-                        <p className="text-base font-medium text-gray-900">{product.price}</p>
+                        </p>
+                        {/* Job description */}
+                        <p className="mt-1.5 mb-1.5 text-sm text-gray-900">{product.description}</p>
+                      </div>
+                      {/* Ratings */}
+                      <div className="flex gap-2 border-t border-gray-200 p-4 font-bold tracking-wider text-gray-700">
+                        <span className="">Rating : </span>
+                        <div className="flex items-center text-gray-600">{product.rating}</div>
+                      </div>
+                      {/* price and hire button */}
+                      <div className="flex justify-between border-t border-gray-200 bg-gray-100 px-4 py-3 uppercase text-black transition">
+                        <button className="categories-page-btn">
+                          <span className="block rounded-sm bg-white px-2 py-3 text-sm font-medium transition hover:bg-transparent">
+                            Buy - {product.buyPrice}
+                          </span>
+                        </button>
+                        <button className="categories-page-btn">
+                          <span className="block rounded-sm bg-white px-2 py-3 text-sm font-medium transition hover:bg-transparent">
+                            Hire - {product.hirePrice}
+                          </span>
+                        </button>
                       </div>
                     </div>
-                  </div>
+                  </article>
                 ))}
               </div>
             </section>
